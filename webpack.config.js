7d2249401fb2272c80bcc30d00253e2676408fc5
Nick Coutsos <nick@coutsos.com>
2017-05-13 18:35:33 -0400

    Update webpack build config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,49 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
-  entry: './src/app.js',
-  output: { path: './build/', filename: 'bundle.js' },
+  entry: path.resolve(__dirname, 'src/app.js'),
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'build')
+  },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015']
-        }
+        exclude: /node_modules/
       },
-      { test: /\.json$/, loader: 'json' }
+      { test: /\.json$/, loader: 'json' },
+      { test: /\.html$/, loader: 'html-loader' },
+      {
+        test: /\.worker\.js$/,
+        use: [
+          { loader: 'worker-loader' },
+          { loader: 'babel-loader' }
+        ]}
+
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function (module) {
+       return (
+         module.context &&
+         module.context
+          .indexOf('node_modules') !== -1
+        )
+      }
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest'
+    })
+  ],
   resolve: {
     alias: {
       'vue$': 'vue/dist/vue.common.js'
@@ -22,9 +52,12 @@ module.exports = {
   externals: {
     'three': 'THREE'
   },
+  devtool: 'source-map',
   devServer: {
     contentBase: './src',
-    historyApiFallback: true
+    historyApiFallback: true,
+    hot: false,
+    inline: false
   },
   node: {
     fs: 'empty'
