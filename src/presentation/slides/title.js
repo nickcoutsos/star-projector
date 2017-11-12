ace1125aa1ec548b9d14cfc3f6beb17b3363cc1c
Nick Coutsos <nick@coutsos.com>
2017-11-11 22:52:20 -0500

    Move title slide styling to revealjs data attributes

--- src/presentation/slides/title.js
+++ src/presentation/slides/title.js
@@ -6,9 +6,12 @@ export const content = (
     data-state="title"
     data-transition="fade-in fade-out"
     data-transition-speed="slow"
-    data-background-position="50% 50%"
+    data-background="radial-gradient(circle at center, darkred 0, rgba(0, 0, 0, 0) 60%), url(assets/hudf_300dpi.jpg)"
+    data-background-size="cover"
   >
     <h1>Star Projector</h1>
-    <h2>A Personal Voyage</h2>
+    <h2 class="fragment" style="transition: 3s ease-in">
+      A Personal Voyage
+    </h2>
   </section>
 )
