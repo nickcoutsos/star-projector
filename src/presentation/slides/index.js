ff218f722745b2b5889b50e00f99619b919f4320
Nick Coutsos <nick@coutsos.com>
2017-11-15 02:04:46 -0500

    .

--- src/presentation/slides/index.js
+++ src/presentation/slides/index.js
@@ -1,11 +1,13 @@
+import * as traversal from './traversal'
 import * as neighbours from './neighbours'
 import * as elements from './elements'
 import * as shadows from './shadows'
 import * as title from './title'
 
 export default [
+  traversal,
+  shadows,
   neighbours,
   elements,
-  shadows,
   title,
 ]
