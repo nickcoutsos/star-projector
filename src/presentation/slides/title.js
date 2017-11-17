4b4033fab882c95b40150119c28f1169996d8163
Nick Coutsos <nick@coutsos.com>
2017-11-17 00:23:19 -0500

    Change presentation's base url

--- src/presentation/slides/title.js
+++ src/presentation/slides/title.js
@@ -6,7 +6,7 @@ export const content = (
     data-state="title"
     data-transition="fade-in fade-out"
     data-transition-speed="slow"
-    data-background="radial-gradient(circle at center, darkred 0, rgba(0, 0, 0, 0) 60%), url(assets/hudf_300dpi.jpg)"
+    data-background="radial-gradient(circle at center, darkred 0, rgba(0, 0, 0, 0) 60%), url(presentation/assets/hudf_300dpi.jpg)"
     data-background-size="cover"
   >
     <h1>Star Projector</h1>
